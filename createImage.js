e0a7fbc892473548702c9dbf6249291d687b8984
Mohamad Khawam <khawammohamad@gmail.com>
2021-12-01 14:37:35 -0500

    init

--- createImage.js
+++ createImage.js
@@ -1,27 +1,29 @@
 const { createCanvas, loadImage } = require("canvas");
 const Vector = require("./vector");
+const fs = require("fs");
 // var argv = require("minimist")(process.argv.slice(2));
 var alpha = randomNumber(1, 10);
 var beta = randomNumber(1, 10);
-var theta = randomNumber(10, 60)
+var theta = randomNumber(10, 60);
 var deta = randomNumber(10, 60);
-var charle = randomNumber(10, 60)
+var charle = randomNumber(10, 60);
+var echo = randomNumber(10, 60);
+var techo = randomNumber(10, 60);
 const seed = Math.random() + Math.random();
 const colorSeed = randomAngle();
 const size = 1;
 const scale = 2000 * Math.random() + 500;
-const offset = 1 * Math.random() + 1;
-var pictures = [];
+const offset = 200 * Math.random() + 1;
+const Name = randomString();
+fs.mkdirSync("./test/" + Name);
 async function createImage(width, height, callback, i) {
-	console.log(`Drawing image ___ ${i}`)
-	if (i == 0) {
-		callback(pictures)
+	console.log(`Drawing image ${Name} ${i}`);
+	if (i <= 0) {
+		return callback();
 	}
-	alpha = alpha - (1 / alpha);
-	beta = beta - (1 / beta);
-	theta = theta - (1 / theta);
-	deta = deta - (1 / deta);
-	charle = charle - (1 / charle);
+	echo -= alpha * (1 / echo);
+	techo -= alpha * (1 / techo);
+	charle -= alpha * (1 / charle);
 
 	const canvas = createCanvas(width, height);
 	const context = canvas.getContext("2d");
@@ -39,10 +41,10 @@ async function createImage(width, height, callback, i) {
 				var value = random(tempx, tempy);
 				vector = vector.addVector(new Vector(value, Math.sin(Math.random())));
 				context.fillStyle = getVectorHex(vector);
-				vector = vector.getStanderedVector()
+				vector = vector.getStanderedVector();
 				context.fillRect(x, y, size, size);
 			}
-			var percent = Math.round(y / height * 100);
+			var percent = Math.round((y / height) * 100);
 			if (percent > past) {
 				past = percent;
 				console.log(`{${percent} %} ${(vector.getAngle() * 180) / Math.PI}  ${vector.getMag()}`);
@@ -52,24 +54,24 @@ async function createImage(width, height, callback, i) {
 
 	createFractial();
 	const buffer = canvas.toBuffer("image/png");
-	pictures.push(buffer);
+	// pictures.push(buffer);
+	fs.writeFileSync("./test/" + Name + `/${Name}_${i}.png`, buffer);
+
 	i = i - 1;
-	createImage(7680, 4320, callback, i)
+	createImage(7680, 4320, callback, i);
 
 	function getVectorHex(vector) {
-		return "#" + Math.floor((getValueFromAngle(vector.getAngle() + colorSeed) * 16777215)).toString(16);
+		return "#" + Math.floor(getValueFromAngle(vector.getAngle() + colorSeed) * 16777215).toString(16);
 	}
-
 }
 function mappingFunction(x) {
-	// return (x / 60) * Math.pow(x, 3) * (30 / (x - 21))
 	var x = 1 / x;
 	return Math.abs((alpha * Math.sin(beta / x) * theta * (x * x * x) + deta * x) / (beta * (x * x) + x));
 }
 function random(x, y) {
 	x = x * seed;
 	y = y * seed;
-	return (alpha * (x * x + y * y)) / (charle * Math.sin(1 / x * y * beta));
+	return (echo * (x * x + y * y)) / (charle * Math.sin((1 / x) * y * techo));
 }
 function getRandomColorHex() {
 	return "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -81,6 +83,15 @@ function randomAngle() {
 	return Math.random() * (Math.PI / 2);
 }
 function randomNumber(x, y) {
-	return (Math.random() * (y - x)) + x;
+	return Math.random() * (y - x) + x;
 }
-module.exports = createImage
+function randomString() {
+	var strings = `abcdefghijklmnopqrstuvwxyz`;
+	var result = "";
+	for (var i = 0; i < 10; i++) {
+		result += strings[Math.floor(randomNumber(0, strings.length - 1))];
+	}
+	console.log(result);
+	return result;
+}
+module.exports = createImage;
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const createImage = require('./createImage');
 var argv = require("minimist")(process.argv.slice(2));
 const fs = require("fs");
 
 if (process.argv[2] == "child") {
-    var i = process.argv[3];
-    createImage(7680, 4320, callback, 15);
-    function callback(buffers) {
-        for (var p = 0; p < buffers.length; p++) {
-            fs.writeFileSync(`./test/test${i}_${p}.png`, buffer);
-        }
-    }
+	var i = process.argv[3];
+	var then = Date.now();
+	require("./createImage")(7680, 4320, callback, 30);
+	function callback() {
+		var now = Date.now();
+		var date = (now - then) / 1000;
+		console.log("done in " + date + "seconds");
+	}
 } else {
-    console.log(process.argv)
-    const cp = require("child_process");
+	console.log(process.argv);
+	const cp = require("child_process");
 
-    for (var i = 0; i < 1; i++) {
-        startChild(i)
-    }
-    function startChild(i) {
-        console.log("hello!");
-        var child = cp.fork(__filename, ["child", i], {
-            stdio: ["inherit", "inherit", "inherit", "ipc"],
-        })
-        child.on("error", (e) => {
-            console.log(e)
-        })
-
-    }
+	for (var i = 0; i < 30; i++) {
+		startChild(i);
+	}
+	function startChild(i) {
+		console.log("hello!");
+		var child = cp.fork(__filename, ["child", i], {
+			stdio: ["inherit", "inherit", "inherit", "ipc"],
+		});
+		child.on("error", (e) => {
+			console.log(e);
+		});
+	}
 }
-
